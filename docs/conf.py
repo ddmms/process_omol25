7a5a484fe06c097af279e8cd8dedce529fae536f
alin m elena <alin@elena.re>
2026-03-22 07:34:21 +0000

    clean up

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,8 @@ project = 'process_omol25'
 copyright = '2026, Alin M Elena'
 author = 'Alin M Elena'
 
-version = "0.1.3"
-release = "0.1.3"
+version = "0.1.4"
+release = "0.1.4"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+version=$1
+
+sed -i.bak "s;^version =.*;version = \"$version\";g" pyproject.toml && rm pyproject.toml.bak
+sed -i.bak "s;^version =.*;version = \"$version\";g" docs/conf.py && rm docs/conf.py.bak
+sed -i.bak "s;^release =.*;release = \"$version\";g" docs/conf.py && rm docs/conf.py.bak
+
+git add pyproject.toml
+git add docs/conf.py
+git commit -m "bump version for release $version"
+git tag -f -a v$version -m "release $version"
+git push
+git push --tags
