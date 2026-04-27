eba72e28b4f8849ea4769314cc16c598369021f9
alin m elena <alin@elena.re>
2026-04-27 07:31:54 +0100

    bump version for release 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,8 @@ project = "lavello_mlips"
 copyright = "2026, Alin M Elena"
 author = "Alin M Elena"
 
-version = "0.1.21"
-release = "0.1.21"
+version = "0.2.0"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
